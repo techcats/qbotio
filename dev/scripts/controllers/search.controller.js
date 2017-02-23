043e9cd7052e4ebd60a17881e2d5592dd6a1e52b
psychobolt <psychobolt@gmail.com>
2017-02-23 15:05:11 -0800

    Update controller with new endpoint

--- dev/scripts/controllers/search.controller.js
+++ dev/scripts/controllers/search.controller.js
@@ -1,9 +1,6 @@
 app.controller('SearchController', ['globals', '$scope', '$http', function (globals, $scope, $http) {
     $scope.getResults = function() {
-        $http({
-            method: 'GET',
-            url: globals.BASE_URL + 'search'
-        }).then(function (response) {
+        $http.get(globals.BASE_URL + 'search/?q=' + ($scope.query ? $scope.query : '')).then(function (response) {
             $scope.results = response.data;
         });
     };
